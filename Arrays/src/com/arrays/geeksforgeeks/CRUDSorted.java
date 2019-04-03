b5b979b5a89a83f70e72646cfff85f5acc6d72c6
sachin <sachinjohar2@gmail.com>
2019-04-03 11:51:23 +0530

    added latest changes

--- Arrays/src/com/arrays/geeksforgeeks/CRUDSorted.java
+++ Arrays/src/com/arrays/geeksforgeeks/CRUDSorted.java
@@ -2,8 +2,9 @@ package com.arrays.geeksforgeeks;
 
 public class CRUDSorted {
 
+    //binary search
     public static int search(int[] arr, int start, int end, int element){
-        while(start < end) {
+        while(start <= end) {
 
             int mid = (start + end)/2;
 
@@ -18,7 +19,15 @@ public class CRUDSorted {
         return -1;
     }
 
+    public static int insert(int[] arr, int element) {
+        return -1;
+    }
+
     public static void main(String[] args) {
+
+        int[] arr = new int[]{1,3,5,7,8,9,11,14};
+
+        System.out.println("Found element at index " +  CRUDSorted.search(arr, 0, arr.length - 1, 14));
 
     }
 }
