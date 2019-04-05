0a0abb2c88f2ebdacde0b934791ee9ccfbf626a5
sachin <sachinjohar2@gmail.com>
2019-04-05 20:43:07 +0530

    new program

--- Arrays/src/com/arrays/geeksforgeeks/PairSumAsX.java
+++ Arrays/src/com/arrays/geeksforgeeks/PairSumAsX.java
@@ -1,8 +1,27 @@
 package com.arrays.geeksforgeeks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PairSumAsX {
 
     public static void checkPairsSum(int[] arr, int x){
 
+        int len = arr.length;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i< len; i++){
+            if(map.containsKey(arr[i])) {
+                System.out.println("("+ map.get(arr[i]) +" , "+ arr[i] +")");
+            }else{
+                map.put(x - arr[i], arr[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{3,5,7,8,9,2,1};
+
+        PairSumAsX.checkPairsSum(arr, 10);
     }
 }
